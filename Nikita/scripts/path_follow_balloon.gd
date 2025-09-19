94e14ce0350642ac2f74cfdb86e2516fff4d34d4
G3rkul3s <morev.ng@mail.ru>
2025-09-19 11:01:23 +0200

    balloon level + ui stretch + waypoint fix

--- Nikita/scripts/path_follow_balloon.gd
+++ Nikita/scripts/path_follow_balloon.gd
@@ -1,4 +1,23 @@
 extends PathFollow3D
 
+var pause_timer : Timer
+var flying  = true
+
+func _ready() -> void:
+	pause_timer = Timer.new()
+	pause_timer.wait_time = 10.0
+	pause_timer.one_shot = true
+	add_child(pause_timer)
+	pause_timer.timeout.connect(_on_timer_timeout)
+
 func _process(delta: float) -> void:
-	progress += 5 * delta
+	if flying:
+		progress += 5 * delta
+		if progress_ratio <= 0.001:
+			#progress_ratio == 1.0
+			flying = false
+			pause_timer.start()
+
+func _on_timer_timeout():
+	flying = true
+	progress_ratio = 0.001001
